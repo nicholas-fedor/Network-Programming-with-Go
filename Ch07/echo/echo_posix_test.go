ebe6af9822f8c76f9e32c882d81a9588ed7fe72d
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2024-02-05 15:07:07 -0700

    Working on echo_posix_test.go

---
+++ Ch07/echo/echo_posix_test.go
@@ -0,0 +1,66 @@
+// Page 149
+// Listing 7-6: Building constraints and imports for macOS and Linux
+package echo
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+// Pages 149-150
+// Listing 7-7: Instantiating the datagram-based echo server.
+func TestEchoServerUnixDatagram(t *testing.T) {
+	dir, err := os.MkdirTemp("", "echo_unixgram")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if rErr := os.RemoveAll(dir); rErr != nil {
+			t.Error(rErr)
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	// Just as with UDP connections, both the server and the client must bind to
+	// an address so they can send and receive datagrams.
+	// The server has its own socket file that is separate from the client's
+	// socket file in Listing 7-8.
+	sSocket := filepath.Join(dir, fmt.Sprintf("s%d.sock", os.Getpid()))
+	serverAddr, err := datagramEchoServer(ctx, "unixgram", sSocket)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cancel()
+
+	err = os.Chmod(sSocket, os.ModeSocket|0622)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Page 150
+	// Listing 7-8: Instantiating the datagram-based echo client.
+	//
+	cSocket := filepath.Join(dir, fmt.Sprintf("c%d.sock", os.Getpid()))
+	client, err := net.ListenPacket("unixgram", cSocket)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// The call to os.Remove in Listing 7-5's datagramEchoServer function cleans
+	// up the socket file when the server closes.
+	// The client has some additional housecleaning, so we make the client clean
+	// up its own socket file when it's all done listening to it.
+	defer func() { _ = client.Close() }()
+
+	// The server should be able to write to the client's socket file as well as
+	// as well as its own socket file, or the server won't be able to reply to
+	// any message.
+	err = os.Chmod(cSocket, os.ModeSocket|0622)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
