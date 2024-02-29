73b7ff614eeac9b53590a4436c5ef9d0d2099488
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2024-02-28 23:12:54 -0700

    Debugged server_test.go

--- Ch09/server_test.go
+++ Ch09/server_test.go
@@ -6,6 +6,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"testing"
@@ -114,7 +115,7 @@ func TestSimpleHTTPServer(t *testing.T) {
 		// empty or you ignore it entirely.
 		// Failure to do so may prevent the client from reusing the underlying
 		// TCP connection.
-		_ = r.Body.Close()
+		_ = resp.Body.Close()
 
 		if c.response != string(b) {
 			t.Errorf("%d: expected %q; actual %q", i, c.response, b)
@@ -125,6 +126,76 @@ func TestSimpleHTTPServer(t *testing.T) {
 	// This causes its Serve method in Listing 9-1 to return, stopping the
 	// server.
 	// The close method abruptly closes client connections.
+	if err := srv.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Copied from reference code at https://github.com/awoodbeck/gnp/blob/master/ch09/server_test.go
+func TestSimpleHTTPServerMethods(t *testing.T) {
+	srv := &http.Server{
+		Addr: "127.0.0.1:8081",
+		Handler: http.TimeoutHandler(
+			handlers.DefaultMethodsHandler(), 2*time.Minute, ""),
+		IdleTimeout:       5 * time.Minute,
+		ReadHeaderTimeout: time.Minute,
+	}
+
+	l, err := net.Listen("tcp", srv.Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		err := srv.Serve(l)
+		if err != http.ErrServerClosed {
+			t.Error(err)
+		}
+	}()
+
+	testCases := []struct {
+		method   string
+		body     io.Reader
+		code     int
+		response string
+	}{
+		{http.MethodGet, nil, http.StatusOK, "Hello, friend!"},
+		{http.MethodPost, bytes.NewBufferString("<world>"), http.StatusOK,
+			"Hello, &lt;world&gt;!"},
+		{http.MethodHead, nil, http.StatusMethodNotAllowed, ""},
+	}
+
+	client := new(http.Client)
+	path := fmt.Sprintf("http://%s/", srv.Addr)
+	for i, c := range testCases {
+		r, err := http.NewRequest(c.method, path, c.body)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+
+		resp, err := client.Do(r)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+
+		if resp.StatusCode != c.code {
+			t.Errorf("%d: unexpected status code: %q", i, resp.Status)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Errorf("%d: %v", i, err)
+			continue
+		}
+		_ = resp.Body.Close()
+
+		if c.response != string(b) {
+			t.Errorf("%d: expected %q; actual %q", i, c.response, b)
+		}
+	}
+
 	if err := srv.Close(); err != nil {
 		t.Fatal(err)
 	}
