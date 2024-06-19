21b1a4b531fe42a60af03969273e7294060ac4da
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2024-06-18 21:43:00 -0700

    Correct imports

--- Ch10/main.go
+++ Ch10/main.go
@@ -5,8 +5,8 @@ import (
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 
 	// Injecting custom modules into Caddy
-	_ "github.com/nicholas-fedor/Network-Programming-with-Go/Ch10/caddy-restrict-prefix/restrictprefix"
-	_ "github.com/nicholas-fedor/Network-Programming-with-Go/Ch10/caddy-toml-adapter/tomladapter"
+	_ "github.com/nicholas-fedor/Network-Programming-with-Go/Ch10/caddy-restrict-prefix"
+	_ "github.com/nicholas-fedor/Network-Programming-with-Go/Ch10/caddy-toml-adapter"
 )
 
 func main() {
